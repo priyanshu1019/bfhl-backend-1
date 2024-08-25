23965de62d4a3e9c7a1b1caab8568a4ba52ec49a
Harshithaareddy <harshithabasireddy2004@gmail.com>
2024-08-25 12:09:34 +0530

    add

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(cors());
 
 app.use(express.json());
 
-const user_id = "BasiReddy_HarshithaReddy_09042005";
-const email = "basi.harshithareddy2021@vitstudent.ac.in";
-const roll_number = "21BDS0294";
+const user_id = "Prianshu_goutam_06122002";
+const email = "prianshu.goutam2021@vitstudent.ac.in";
+const roll_number = "21BCE0400";
 
 // POST /bfhl endpoint
 app.post('/bfhl', (req, res) => {
